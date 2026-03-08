6ff9c394a7e6caf6e18f7b2bc257b388f10cb08b
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2026-03-09 00:58:43 +0100

    Added smooth scroll js script

---
+++ assets/js/smooth_scroll.js
@@ -0,0 +1,14 @@
+// Add smooth scroll to page on load
+$(document).ready(function(){
+	$(".navbar .nav-link").on('click', function(event){
+		if (this.hash !== ""){
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 700, function(){
+				window.location.hash = hash;
+			});
+		}
+	});
+});
