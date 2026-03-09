898003f14e9a7b1fa21db94689f6062e4ee87730
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2026-03-09 02:21:04 +0100

    Added code to generate team member profile entry

--- assets/js/docgen.js
+++ assets/js/docgen.js
@@ -11,3 +11,29 @@ function docgen_writeCopyrightDate(baseYear, siteURL) {
 	
 	document.write(", <a href=\"" + siteURL + "\" target=\"_blank\"> Genuine Mercenary Games </a> All rights reserved");
 }
+
+function docgen_writeProfile(name, picture, description, socials) {
+	let socialsString = "";
+	for(let i = 0; i < socials.length; ++i) {
+		let s_img = socials[i].img;
+		let s_url = socials[i].url;
+		socialsString += `
+			<a href="${s_url}" target="_blank" class="px-2"><img src="${s_img}" class="custom_rrss_btn"></a>
+		`
+	}
+	let s = `
+	<div class="col-md-4 my-3">
+		<div class="team-wrapper text-center">
+			<img src="${picture}" class="circle-120 rounded-circle mb-3 shadow" alt="${name}">
+			<h5 class="my-3">${name}</h5>
+			<p>${description}</p>
+			<h6 class="socials mt-3">
+				<center>
+					${socialsString}
+				</center>
+			</h6>
+		</div>
+	</div>
+	`;
+	document.write(s);
+}
